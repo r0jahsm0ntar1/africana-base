3bc96552c69018220922980a7bde6abb6593215f
Rojahs Montari <rojahsmontari@gmail.com>
2025-09-09 13:59:09 +0300

    With great power comes great responsibility.
    The Africana Framework is a software designed for network & web hacking by automating as much stuff as possible to detect vulnerabilities on most common services and web technologies. It also has some wide range of penetration testing from internal network, wireless networks, system anonymity to web bug hunting. It's purely written for Good and not Evil.

--- networks/caplets/steal-cookies/README.md
+++
@@ -1,4 +0,0 @@
-# Steal cookies
-
-Enumerate each domain from file and steal all cookies without `Secure` flag.
-
--- phishers/set/setup.py
+++ phishers/set/setup.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 import subprocess
 import os
 print("[*] Installing requirements.txt...")
-subprocess.Popen("pip3 install -r requirements.txt", shell=True).wait()
+#subprocess.Popen("pip3 install -r requirements.txt", shell=True).wait()
 print("[*] Installing setoolkit to /usr/local/share/setoolkit")
 print(os.getcwd())
 subprocess.Popen("mkdir /usr/local/share/setoolkit/;mkdir /etc/setoolkit/;cp -rf * /usr/local/share/setoolkit/;cp src/core/config.baseline /etc/setoolkit/set.config", shell=True).wait()
